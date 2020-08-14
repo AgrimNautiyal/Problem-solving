67845df5dccf760b9c56eeece5b08a73b40eabb5
unknown <agrimnautiyal1999@gmail.com>
2020-08-15 00:51:14 +0530

    remove loop in linked list using a hashing approach

---
+++ linked_list/remove_loop_using_hashing.py
@@ -0,0 +1,15 @@
+def removeLoop(head):
+    # code here
+    # remove the loop without losing any nodes
+    tmp = head
+    s=set()
+    s.add(tmp)
+    while(tmp.next):
+        if tmp.next not in s:
+            s.add(tmp.next)
+            tmp = tmp.next
+        else:
+            break
+    
+    if tmp.next:
+        tmp.next=None
