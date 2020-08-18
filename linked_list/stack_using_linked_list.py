74f65b18a09ae3356ddb05f8f3fedbbfd33cb7c4
unknown <agrimnautiyal1999@gmail.com>
2020-08-19 01:08:23 +0530

    Added code for implementing stack using linked list

---
+++ linked_list/stack_using_linked_list.py
@@ -0,0 +1,55 @@
+# Class to represent a node
+class StackNode:
+
+	# Constructor to initialize a node
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+
+class Stack:
+
+    # The method push to push element into
+    # the stack
+    def __init__(self):
+        self.head = None
+        
+    def push(self, data):
+
+        # Add code here
+
+        # The method pop which return the element
+        # poped out of the stack
+        if self.head is None:
+            self.head=  StackNode(data)
+        else:
+            tmp = self.head
+            
+            while(tmp.next):
+                tmp = tmp.next
+            tmp.next = StackNode(data)
+
+    def pop(self):
+        
+        
+        if self.head is None:
+            return -1
+        if self.head.next is None:
+            data = self.head.data
+            self.head =None
+            return data
+        tmp = self.head
+        # Add code here
+        while(tmp.next.next):
+            tmp=tmp.next
+        data = tmp.next.data
+        tmp.next = None
+        return data
+
+    def view(self):
+        l = []
+        tmp = self.head
+        while(tmp):
+            l.append(tmp.data)
+            tmp = tmp.next
+        return l
